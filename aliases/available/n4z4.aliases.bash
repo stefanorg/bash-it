0b8c51c7d2a95405b0f764136c6d481e8bf0a14b
stefanorg <stefanorg@gmail.com>
2014-01-06 11:42:04 +0100

    custom alias per utilizzare gls (gnu ls, installato tramite brew install coreutile)

---
+++ aliases/available/n4z4.aliases.bash
@@ -0,0 +1,11 @@
+cite about-alias
+about-alias 'n4z4 aliases'
+
+# List directory contents
+alias ls='gls --color=auto'        # Compact view, show colors
+
+
+if [ $(uname) = "Linux" ]
+then
+  alias ls="ls --color=auto"
+fi
